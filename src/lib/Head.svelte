00f4eb894e9f635757c333c3f3f713e703d1707c
fabezio <fabezio@outlook..fr>
2021-08-31 20:44:46 +0200

    mise en place

---
+++ src/lib/Head.svelte
@@ -0,0 +1,9 @@
+<script >
+    export let url = "Plannings Arcs de Seine"
+    export let pageTitle
+</script>
+
+<svelte:head>
+	<title>{url} | {pageTitle}</title>
+</svelte:head>
+<h1>{pageTitle}</h1>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,30 @@
+
+
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import Footer from '$lib/containers/Footer.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<Footer />
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	
+</style>
